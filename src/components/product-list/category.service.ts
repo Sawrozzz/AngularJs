cb133dea049b58e6778d83d39d3640e840e7aa0e
[saroj adhikari] <sawroz.adhikari@gmail.com>
2024-05-10 08:33:33 +0545

    category api fetched

---
+++ src/components/product-list/category.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, map } from 'rxjs';
+import { Category } from './Category';
+//import { HttpErrorHandler, HandleError } from '../http-error-handler.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'my-auth-token',
+  }),
+};
+
+@Injectable()
+export class CategoryService {
+  CategoryUrl = 'api/Category';
+  constructor(private http: HttpClient) {}
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(
+      'https://localhost:7055/api/Categorie'
+    );
+  }
+}
--- src/components/product-list/product-list.component.ts
+++ src/components/product-list/product-list.component.ts
@@ -1,61 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { NgFor } from '@angular/common';
 import { CommonModule } from '@angular/common';
-import { Category } from './category';
+import { Category } from './Category';
+import { CategoryService } from './category.service';
+
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [NgFor, CommonModule],
+  providers: [CategoryService],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
-
-
-
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  constructor(private categoryService: CategoryService) {}
   title = 'product list will display here ';
-  categories: Category[] =[
-    {
-      id:1,
-      name:'car'
-    },
-    {
-      id:2,
-      name:'laptop'
-    }
-  ];
+  categories: Category[] = [];
+  isLoading:boolean=true;
   products: Product[] = [
-    {
-      id: 1,
-      title: 'Iphone',
-      description: 'Iphone 12 pro max best phone',
-      price: 130000,
-    },
-    {
-      id: 2,
-      title: 'Samsung',
-      description: 'Samsung s24 max best phone',
-      price: 100000,
-    },
-    {
-      id: 3,
-      title: 'Redmi',
-      description: 'Redmi note 7 pro best phone in low price',
-      price: 30000,
-    },
-    {
-      id: 5,
-      title: 'iphone 15',
-      description: 'Redmi note 7 pro best phone in low price',
-      price: 30000,
-    },
+    // {
+    //   id: 1,
+    //   title: 'Iphone',
+    //   description: 'Iphone 12 pro max best phone',
+    //   price: 130000,
+    // },
+    // {
+    //   id: 2,
+    //   title: 'Samsung',
+    //   description: 'Samsung s24 max best phone',
+    //   price: 100000,
+    // },
+    // {
+    //   id: 3,
+    //   title: 'Redmi',
+    //   description: 'Redmi note 7 pro best phone in low price',
+    //   price: 30000,
+    // },
+    // {
+    //   id: 5,
+    //   title: 'iphone 15',
+    //   description: 'Redmi note 7 pro best phone in low price',
+    //   price: 30000,
+    // },
   ];
   isVisible = true;
   toggleShow() {
     this.isVisible = !this.isVisible;
   }
+  // data
+  ngOnInit() {
+    this.getCategories();
+  }
+
+  getCategories(): void {
+    this.isLoading = true;
+    this.categoryService.getCategories().subscribe(
+      (data) => {
+        console.log(data);
+        this.categories = data;
+        this.isLoading = false;
+      },
+      (error) => {
+        this.isLoading = false;
+        console.log('Error:', error);
+      }
+    );
+  }
+
   getDummy(): string {
     return 'This is dummy content';
   }
